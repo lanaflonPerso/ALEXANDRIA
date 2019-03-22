6a27a90764d95fcb03e2d85d8b1bb59bfab662d4
yanomami <yanomami92@gmail.com>
2019-03-23 00:38:37 +0100

    misc

--- alexandria_webapp/src/main/java/com/alexandria/servlets/About.java
+++ alexandria_webapp/src/main/java/com/alexandria/servlets/About.java
@@ -2,13 +2,17 @@ package com.alexandria.servlets;
 
 import java.io.IOException;
 
-@javax.servlet.annotation.WebServlet(name = "About")
+@javax.servlet.annotation.WebServlet("/about")
 public class About extends javax.servlet.http.HttpServlet {
+
+    public static final String VIEW  = "/WEB-INF/jsp/about.jsp";
+
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
+        this.getServletContext().getRequestDispatcher(VIEW).forward(request, response);
     }
 }
