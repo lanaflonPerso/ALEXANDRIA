05e65d97be184fff14ded6f03754d3240bb527f3
yanomami <yanomami92@gmail.com>
2019-03-23 11:40:40 +0100

    misc

---
+++ alexandria_webapp/src/main/java/com/alexandria/config/EncodingFilter.java
@@ -0,0 +1,21 @@
+package com.alexandria.config;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+  * Servlet Filter implementation class EncodingFilter
+  */
+@WebFilter("/EncodingFilter")
+public class EncodingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+}
