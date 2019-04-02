1fdc93b228ee56a172eda7d5887a1c2ba3451f10
yanomami <yanomami92@gmail.com>
2019-04-02 15:05:55 +0200

    misc

---
+++ alexandria-webapp/src/main/java/com/alexandria/controllers/RegistrationController.java
@@ -0,0 +1,37 @@
+package com.alexandria.controllers;
+
+import com.alexandria.entities.ClientEntity;
+import com.alexandria.managers.ClientManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class RegistrationController {
+
+  @Autowired
+  ClientManager clientManager;
+
+  @RequestMapping(value = "/register", method = RequestMethod.GET)
+  public ModelAndView showRegister(HttpServletRequest request, HttpServletResponse response) {
+    ModelAndView mav = new ModelAndView("register");
+    mav.addObject("client", new ClientEntity());
+
+    return mav;
+  }
+
+  @RequestMapping(value = "/registerProcess", method = RequestMethod.POST)
+  public ModelAndView addUser(HttpServletRequest request, HttpServletResponse response,
+                              @ModelAttribute("client") ClientEntity client) {
+
+    clientManager.register(client);
+
+    return new ModelAndView("welcome", "firstname", client.getFirstName());
+  }
+}
