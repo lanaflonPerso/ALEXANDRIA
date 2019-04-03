e0e77c601c2e78a7c167863d03955879bc5085f7
yanomami <yanomami92@gmail.com>
2019-04-03 16:16:15 +0200

    misc

--- alexandria-business/src/main/java/com/alexandria/managers/ClientManagerImpl.java
+++ alexandria-business/src/main/java/com/alexandria/managers/ClientManagerImpl.java
@@ -29,6 +29,9 @@ public class ClientManagerImpl implements ClientManager {
 
     @Override
     public void register(ClientEntity client) {
+
+        // TODO : Check that the client email doesn't already exist in database before record the client.
+
         clientDao.create(client);
     }
 
