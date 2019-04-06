0b08a5ee31344a95d03597eb2c9b2afe310859d5
yanomami <yanomami92@gmail.com>
2019-04-06 18:02:29 +0200

    misc

--- alexandria-webapp/src/main/java/com/alexandria/controllers/ProductsController.java
+++ alexandria-webapp/src/main/java/com/alexandria/controllers/ProductsController.java
@@ -1,7 +1,10 @@
 package com.alexandria.controllers;
 
 import com.alexandria.entities.CategoryEntity;
+import com.alexandria.managers.Cart;
 import com.alexandria.managers.ProductManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +13,12 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Controller
 public class ProductsController {
+
+    private static final Logger logger = LogManager.getLogger(ProductsController.class);
 
     @Autowired
     ProductManager productManager;
@@ -35,6 +38,19 @@ public class ProductsController {
         mav.addObject("productsList", productManager.getProductsList());
         mav.addObject("categoryList", categoryList);
 // filtrer les produits par catégorie : https://openclassrooms.com/forum/sujet/jquery-filtrer-les-produits-par-categorie
+
+        // TODO ----------------------------------------------------
+        // Get user cart session
+        Cart userCartSession = (Cart)request.getSession().getAttribute( "userCartSession");
+        mav.addObject("userCartSession", userCartSession);
+        //  TODO:
+        //   -> Utiliser "userCartSession" dans products.jsp pour le databinding
+        //      -> ATTENTION au null si le "userCartSession" n'a pas été initialisé lors du login/register
+        //          -> pas d'accès à products.jsp tant que l'utilisateur n'est pas loggé (cf. header.jspf )
+        //      -> On pourrait se passer ici de la récupération du "userCartSession" en le passant directement au ModelAndView lors de la redirection de "login/register" vers "products"
+        //   -> Récupérer "userCartSession" dans la méthode POST du ProductsController via l'argument "@ModelAttribute("userCartSession") Cart userCartSession"
+        // TODO ----------------------------------------------------
+
         return mav;
     }
 
